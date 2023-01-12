fe4c4643653756aa04d7280786dd940c6e9696e6
Michael Lenaghan <michaell@dazzit.com>
2023-01-12 13:59:45 -0500

    Change `collections.Mapping` to `collections.abc.Mapping` in `deep_update.py`.

--- oldtoronto/utils/deep_update.py
+++ oldtoronto/utils/deep_update.py
@@ -5,7 +5,7 @@ def deep_update(d, u):
     """Do a deep, in-place update of d with u."""
     # See https://stackoverflow.com/a/3233356/388951
     for k, v in u.items():
-        if isinstance(v, collections.Mapping):
+        if isinstance(v, collections.abc.Mapping):
             d[k] = deep_update(d.get(k, {}), v)
         else:
             d[k] = v
